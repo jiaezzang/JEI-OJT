55d793c30953ca7da0ecccdc1a6dac3660e74d45
jiaezzang <visveryver2@gmail.com>
2023-02-15 09:53:09 +0900

    버튼 클릭 및 페이지 이동

--- svg/dynamic/main.js
+++ svg/dynamic/main.js
@@ -1,16 +1,45 @@
-const answer = document.querySelector(".answer");
+const answer = document.querySelector('.answer');
 const next = document.querySelector('#next');
 
-//페이지 생성 시점에 모든 버튼 가져와서 onclick이벤트 걸어준다.
+//페이지 생성 시점에 모든 버튼 가져와서 onclick이벤트
 window.onload = () => {
     for(let i=0; i<=9; i++) {
+        //모든 버튼 생성
         let btn = document.querySelector('.btn' + i);
-        btn.onclick = gTagClick;
+        btn.onclick = btnClick;
+    }
+    hideAnswer(1, 8)
+}
+
+//모든 답안 가리기
+function hideAnswer(start, end){
+    for(let i=start; i<=end; i++){
+        let gTag = document.querySelector('#num' + i);
+        gTag.querySelector('.answer').style.display = 'none';
     }
 }
 
-// 태그 클릭시 실행 이벤트
-function gTagClick() {
-    let box = this.querySelector("rect");
-    box.setAttribute("fill", "darkgray");
+// 버튼 클릭시 실행 이벤트
+function btnClick() {
+    let box = this.querySelector('rect');
+    box.setAttribute('fill', 'darkgray');
+}
+
+//다음 문제로 넘어가는 버튼 
+next.onclick =(e) => {
+    const question = document.querySelector('#currentPage');
+    const id = question.getAttribute('href');
+    const nextNum = Number([...id].pop()) + 1;
+    if(nextNum < 9){
+        question.setAttribute('href', '#num' + nextNum)
+    }
+    resetBtn()
+}
+
+//버튼 리셋
+function resetBtn() {
+    for(let i=0; i<=9; i++) {
+        let btn = document.querySelector('.btn'+i);
+        btn.querySelector('rect').setAttribute('fill', 'lightgray')
+    }
 }
