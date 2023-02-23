09565bf3b8b796d6045b1959d4bd55e6a6977ef3
JIAE-K <2023a0015@jei.com>
2023-02-23 09:23:20 +0900

    원 회전

--- svg/drawCircle/main.js
+++ svg/drawCircle/main.js
@@ -66,7 +66,7 @@ tryBtn.addEventListener("mousedown", (event) => {
         drag = true;
         circle.style.dispaly = "";
     }
-    
+
     if(bestLength === Infinity){
       const buttonAudio = new Audio('./audio/button-09a.mp3');
       buttonAudio.play();
@@ -79,43 +79,27 @@ document.addEventListener("mousemove",(event) => {
 
 document.addEventListener("mouseup", (event) => {
     drag = false;
-
 });
-
-
-//조건에 해당할 시 효과음 이벤트
-function soundEffect() {
-  if(circle.style.strokeDashoffset < 2.5){
-    const magicAudio = new Audio('./audio/magic-chime-01.mp3');
-    magicAudio.play();
-    document.querySelector("#wait").style.display = "";
-    removeEvent();
-  }
-}
-
-//효과음 이벤트가 한번만 실행되도록 이벤트 제거
-svg.addEventListener('mouseup', soundEffect);
-function removeEvent(){
-  svg.removeEventListener('mouseup', soundEffect);
-}
-
 
 //마우스를 드래그 할 때 마우스 좌표를 받아와 넘주는 함수
 function mMove(event){
-    if(drag === true){
-      document.querySelector("#try").style.display = "none";
-      alert_coords(event);
-      let now = [stX, stY];
+  if(drag === true){
+    document.querySelector("#try").style.display = "none";
+    alert_coords(event);
+    let now = [stX, stY];
 
-      mousemoved(now);
-      //진행 시 +값으로만 나아가도록
-      if(bestLength - beforeValue >= 0 && bestLength - beforeValue < 90){
-        progress(bestLength);
-      }
-      if(circle.style.strokeDashoffset < 10){
-        circle.style.strokeDashoffset = 0;
-      }
+    mousemoved(now);
+    //진행 시 +값으로만 나아가도록
+    if(bestLength - beforeValue >= 0 && bestLength - beforeValue < 90){
+      progress(bestLength);
     }
+    //끝지점에 다다랐을 때 마무리
+    if(circle.style.strokeDashoffset < 10){
+      circle.style.strokeDashoffset = 0;
+      controller.setAttribute("cx", 600);
+      controller.setAttribute("cy", 130);
+    }
+  }
 }
 
 // 가장 가까운 점 찾기
@@ -173,14 +157,30 @@ function closestPoint(pathNode, point) {
   }
 }
 
-const speaker = document.querySelector("#speaker");
 //텍스트 읽기
+const speaker = document.querySelector("#speaker");
+
 speaker.onclick = () => {
   const voice = new Audio('./audio/voice.mp3');
   voice.play();
   speaker.setAttribute("fill", "#D2B48C");
-  console.log(voice);
   setTimeout(function(){
       speaker.setAttribute("fill", "orange");
   }, 1500);
+}
+
+//조건에 해당할 시 효과음 이벤트
+function soundEffect() {
+  if(circle.style.strokeDashoffset < 2.5){
+    const magicAudio = new Audio('./audio/magic-chime-01.mp3');
+    magicAudio.play();
+    document.querySelector("#wait").style.display = "";
+    removeEvent();
+  }
+}
+
+//효과음 이벤트가 한번만 실행되도록 이벤트 제거
+svg.addEventListener('mouseup', soundEffect);
+function removeEvent(){
+  svg.removeEventListener('mouseup', soundEffect);
 }
