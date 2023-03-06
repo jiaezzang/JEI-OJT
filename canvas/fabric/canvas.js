586819b2681d73270f73f2abcda15592c0ffb223
JIAE-K <2023a0015@jei.com>
2023-03-06 16:03:58 +0900

    정답을 모두 골랐을 때 오답표시가 뜨지 않게 함

--- canvas/fabric/canvas.js
+++ canvas/fabric/canvas.js
@@ -7,7 +7,8 @@ const triangle1 = new fabric.Triangle({
     stroke: 'red',
     fill: 'rgba(0,0,0,0)',
     width: 60,
-    height: 60
+    height: 60,
+    selectable: false
 });
 
 const triangle2 = new fabric.Triangle({
@@ -17,25 +18,32 @@ const triangle2 = new fabric.Triangle({
     fill: 'rgba(0,0,0,0)',
     angle: 50,
     width: 150,
-    height: 40
+    height: 40,
+    selectable: false
 });
 
 //Path로 도형 그리기 
 const triangle3 = new fabric.Path('M 0 0 L 130 80 L 120 130 z');
 triangle3.set({ left: 500, top: 80 });
 triangle3.set({ fill:'rgba(0,0,0,0)', stroke: 'blue'});
+triangle3.set('selectable', false);
 
 const path1 = new fabric.Path('M 0 0 L 20 80 L 70 7 L 60 100');
 path1.set({ left: 300, top: 100});
 path1.set({ fill:'rgba(0,0,0,0)', stroke: 'orange'});
+path1.set('selectable', false);
+
 
 const path2 = new fabric.Path('M 0 50 L 70, 0 L 70, 50 L 0, 60 ')
 path2.set({ left: 280, top: 250});
 path2.set({ fill:'rgba(0,0,0,0)', stroke: 'yellowgreen'});
+path2.set('selectable', false);
 
 const path3 = new fabric.Path('M 0 50 L 70, 70 L 70, 10 L 0, 40 z')
 path3.set({ left: 500, top: 250});
 path3.set({ fill:'rgba(0,0,0,0)', stroke: 'pink'});
+path3.set('selectable', false);
+
 
 //rect로 도형 그리기
 const rect = new fabric.Rect({
@@ -44,7 +52,8 @@ const rect = new fabric.Rect({
     stroke: 'violet',
     fill: 'rgba(0,0,0,0)',
     width: 40,
-    height: 40
+    height: 40,
+    selectable: false
 });
 
 //circle로 도형 그리기 
@@ -54,14 +63,16 @@ const circle = new fabric.Circle({
     fill: 'rgba(0,0,0,0)',
     radius: 30,
     left: 170,
-    top: 200
+    top: 200,
+    selectable: false
 })
 
 //text 작성하기
 const guide = new fabric.Text('삼각형을 모두 찾아 고르세요',{
     fontSize: 28,
     left: 130,
-    top: 30
+    top: 30,
+    selectable: false
 });
 
 //이미지 사용하기
@@ -76,17 +87,8 @@ const speaker = fabric.Image.fromURL(speakerImg.src, function(oImg) {
     canvas.add(oImg);
 });
 
-//선택 불가능 처리
-canvas.selection = false; 
-triangle1.set('selectable', false);
-triangle2.set('selectable', false);
-triangle3.set('selectable', false);
-path1.set('selectable', false);
-path2.set('selectable', false);
-path3.set('selectable', false);
-rect.set('selectable', false);
-circle.set('selectable', false);
-guide.set('selectable', false);
+
+
 
 //커서 스타일 설정
 canvas.hoverCursor = 'pointer'
@@ -153,19 +155,19 @@ canvas.on('mouse:down', function(option) {
 
 //오답 선택 시 애니메이션
 canvas.on('mouse:down', function(option) {
-    if(option.target === path1){
+    if(option.target === path1 && (a || b || c)){
         audio.play('beep.mp3');
         checkImg(280, 90, 0);
-    } else if(option.target === path2){
+    } else if(option.target === path2 && (a || b || c)){
         audio.play('beep.mp3');
         checkImg(260, 270, 0);
-    } else if(option.target === path3){
+    } else if(option.target === path3 && (a || b || c)){
         audio.play('beep.mp3');
         checkImg(480, 260, 0);
-    } else if(option.target === circle){
+    } else if(option.target === circle && (a || b || c)){
         audio.play('beep.mp3');
         checkImg(130, 200, 0);
-    } else if(option.target === rect){
+    } else if(option.target === rect && (a || b || c)){
         audio.play('beep.mp3');
         checkImg(430, 110, 0);
     }
