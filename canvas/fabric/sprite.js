a6302a2b8ee95ae15f7143abb66425548e0fc42f
JIAE-K <2023a0015@jei.com>
2023-03-06 14:23:33 +0900

    스프라이트 애니메이션 작업 중

--- canvas/fabric/sprite.js
+++ canvas/fabric/sprite.js
@@ -1,4 +1,42 @@
-const confetti = new Image();
-confetti.src = './img/conffeti.png';
-confetti.frameCount = 8;
+const dogRun = new Image();
+dogRun.src = './img/dog_run.png';
+dogRun.frameCount = 8;
+
+const dogIdle = new Image();
+dogIdle.src = './img/dog_idle.png';
+dogIdle.frameCount = 10;
+
+let i = 4;
+let currFrame = 0;
+let z = 0;
+
+const animation = () => {
+
+    const RunSprite = fabric.Image.fromURL(dogRun.src, (oImg) => {
+        oImg.scale(0.25);
+        oImg.set({left: 30, top: 300, cropX: dogRun.naturalWidth/8 * currFrame});
+        oImg.selectable = false;
+        // oImg.animate('left', '+=200', {
+        //     onChange: canvas.renderAll.bind(canvas),
+        //     duration: 2000,
+        // })
+        canvas.add(oImg);
+    },{
+        width: dogRun.naturalWidth/dogRun.frameCount
+    });
+
+    z++;
+    if(z === 8){
+        currFrame += 1;
+        z = 0;
+    }
+    if(currFrame === dogRun.frameCount){
+        currFrame = 0;
+    };
+
+    requestAnimationFrame(animation);
+}
+
+
+animation();
 
