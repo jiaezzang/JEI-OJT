0a34898a0b200275997dfa911eaf42f6b999d5ba
jiaezzang <visveryver2@gmail.com>
2023-03-10 16:54:59 +0900

    기본 구조 생성

--- ReactApp/vite-project/src/pages/Main/Main.tsx
+++ ReactApp/vite-project/src/pages/Main/Main.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Main() {
   return (
-    <div>Main</div>
+    <div className="iframebox width-3x md:width-5xl"> 
+    <iframe src="http://127.0.0.1:5500/canvas/guideAnimation/index.html" width="1010" height="635" scrolling='no' allowFullScreen></iframe> 
+    {/* //컨텐츠를 띄워놓고 불러오는 방식으로  */}
+    </div>
+
   )
 }
