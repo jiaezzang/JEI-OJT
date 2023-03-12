cd11279ad42e41ca025c6bce4c737e40dd0e2b66
jiaezzang <visveryver2@gmail.com>
2023-03-12 22:35:13 +0900

    로그인 기본 폼 생성

--- ReactApp/vite-project/src/pages/SignIn/SignIn.tsx
+++ ReactApp/vite-project/src/pages/SignIn/SignIn.tsx
@@ -2,6 +2,21 @@ import React from 'react'
 
 export default function SignIn() {
   return (
-    <div className="width-3x md:width-5xl">SignIn</div>
+    <div className="width-3x md:width-5xl"
+    style={{
+      display: 'flex', justifyContent: 'center', alignItems: 'center', 
+      width: '100%', height: '100vh'
+    }}>
+      <form style={{display: 'flex', flexDirection: 'column'}}>
+        <label>아이디</label>
+        <input type="text"/>
+        <label>비밀번호</label>
+        <input type="password" />
+        <br />
+        <button formAction=''>
+          로그인
+        </button>
+      </form>
+    </div>
   )
 }
