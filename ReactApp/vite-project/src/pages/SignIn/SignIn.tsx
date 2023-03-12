ffbee0db753909f236203a8113a2b6e96c86c889
JIAE-K <2023a0015@jei.com>
2023-03-12 22:53:53 +0900

    로그인폼 border 설정

--- ReactApp/vite-project/src/pages/SignIn/SignIn.tsx
+++ ReactApp/vite-project/src/pages/SignIn/SignIn.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
 
 export default function SignIn() {
+  
   return (
-    <div className="width-3x md:width-5xl"
+    <div className="xs:width-3x md:width-5xl"
     style={{
       display: 'flex', justifyContent: 'center', alignItems: 'center', 
       width: '100%', height: '100vh'
     }}>
       <form style={{display: 'flex', flexDirection: 'column'}}>
         <label>아이디</label>
-        <input type="text"/>
+        <input type="text" style={{border: '2px solid black'}}/>
         <label>비밀번호</label>
-        <input type="password" />
+        <input type="password" style={{border: '2px solid black'}}/>
         <br />
-        <button formAction=''>
+        <button formAction='' style={{border: 'solid orange', background: 'orange'}}>
           로그인
         </button>
       </form>
