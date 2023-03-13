b808e18e46e49d697124a6ac2f098ed8e6dfc35c
jiaezzang <visveryver2@gmail.com>
2023-03-13 11:24:18 +0900

    임시 아이디, 비밀번호 생성

--- ReactApp/vite-project/src/pages/SignIn/SignIn.tsx
+++ ReactApp/vite-project/src/pages/SignIn/SignIn.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
+const users = [
+  {id: "jiae22", password: "1234", name:"jiae"},
+  {id: "jiae33", password: "3456", name:"kim"},
+  {id: "jiae44", password: "4567", name:"diae"},
+];
+
+console.log(users[1])
 export default function SignIn() {
+
   return (
     <div
-      className="flex items-center justify-center w-full h-screen px-4"
-      style={{ backgroundColor: '#f9f9f9' }}
-    >
+      className="flex items-center justify-center w-full h-screen px-4 bg-blue-100">
       <form className="bg-white rounded-lg shadow-lg p-6 w-full md:w-2/3 lg:w-1/2 xl:w-1/3">
         <h2 className="text-2xl font-bold mb-6 text-center">로그인</h2>
         <div className="mb-4">
@@ -31,7 +37,7 @@ export default function SignIn() {
           />
         </div>
         <button
-          className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded w-full"
+          className="bg-blue-500 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded w-full"
           type="submit"
         >
           로그인
