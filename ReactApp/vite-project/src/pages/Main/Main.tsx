8c7430a574598cdbbcf0575eab839e227f8104db
JIAE-K <2023a0015@jei.com>
2023-03-13 14:44:32 +0900

    버튼 클릭 시 컨텐츠 변경

--- ReactApp/vite-project/src/pages/Main/Main.tsx
+++ ReactApp/vite-project/src/pages/Main/Main.tsx
@@ -3,17 +3,16 @@ import { useState } from 'react';
 
 interface ButtonProps {
   label: string;
-  onClick?: () => void;
+  onClick?: (e: any) => void;
+  value: string;
 }
 
-
-const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
-
+const Button: React.FC<ButtonProps> = ({ label, onClick, value }) => {
 
   return (
     <button
     className="bg-blue-500 rounded-xl shadow-lg px-4 py-2 mr-2 text-white"
-      onClick={onClick}
+      onClick={onClick} value={value}
     >
       {label}
     </button>
@@ -22,13 +21,19 @@ const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
 
 export default function Main() {
 
+  const [content, setContent] = useState('');
+
+  const btnHandler = (e: any) => {
+    setContent(e.currentTarget.value);
+  }
+
   return (
     <div
       className="flex items-center justify-center w-full h-screen px-4 bg-blue-100">
-      <div className="p-4 md:p-8 w-full flex flex-col items-center max-w-screen-md">
-        <div className="w-full h-0 relative" style={{ paddingTop: '56.25%' }}>
+      <div className="p-2 md:p-2 w-full flex flex-col items-center max-w-screen-md">
+        <div className="w-full h-0 relative" style={{ paddingTop: '62.7%' }}>
           <iframe
-            src="http://127.0.0.1:5500/ReactApp/vite-project/public/content2/index.html"
+            src={content}
             className="absolute top-0 left-0 w-full h-full shadow-lg bg-white"
             frameBorder="0"
             scrolling="no"
@@ -36,10 +41,10 @@ export default function Main() {
           ></iframe>
         </div>
         <div className="flex mt-4" >
-          <Button label="튜토리얼" />
-          <Button label="더하기" />
-          <Button label="원 그리기" />
-          <Button label="삼각형 찾기" />
+          <Button label="튜토리얼" onClick={btnHandler} value="http://127.0.0.1:5500/ReactApp/vite-project/public/content3/index.html"/>
+          <Button label="더하기" onClick={btnHandler} value="http://127.0.0.1:5500/ReactApp/vite-project/public/content1/index.html"/>
+          <Button label="원 그리기" onClick={btnHandler} value="http://127.0.0.1:5500/ReactApp/vite-project/public/content2/index.html"/>
+          <Button label="삼각형 찾기" onClick={btnHandler} value="http://127.0.0.1:5500/ReactApp/vite-project/public/content4/index.html"/>
         </div>
       </div>
     </div>
