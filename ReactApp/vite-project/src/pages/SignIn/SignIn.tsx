d5a0e46e1bc4292016e03ee34215ec5d2408adc3
jiaezzang <visveryver2@gmail.com>
2023-03-13 13:08:43 +0900

    로그인 기능 구현

--- ReactApp/vite-project/src/pages/SignIn/SignIn.tsx
+++ ReactApp/vite-project/src/pages/SignIn/SignIn.tsx
@@ -1,18 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const users = [
-  {id: "jiae22", password: "1234", name:"jiae"},
-  {id: "jiae33", password: "3456", name:"kim"},
-  {id: "jiae44", password: "4567", name:"diae"},
+  { id: 'jiae22', password: '1234', name: '지애' },
+  { id: 'jiae33', password: '3456', name: 'jiae' },
+  { id: 'jiae44', password: '4567', name: 'kim' },
 ];
 
-console.log(users[1])
 export default function SignIn() {
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const onIdHandler = (e: any) => {
+    setId(e.currentTarget.value);
+  };
+
+  const onPasswordHandler = (e: any) => {
+    setPassword(e.currentTarget.value);
+  };
+
+  const onSubmitHandler = (e: any) => {
+    e.preventDefault();
+    const user = users.find((u) => u.id === id && u.password === password);
+    if (user) {
+      console.log(`${user.name}님 환영합니다`);
+    } else {
+      setErrorMessage('아이디 또는 패스워드를 확인해 주세요');
+    }
+  };
 
   return (
-    <div
-      className="flex items-center justify-center w-full h-screen px-4 bg-blue-100">
-      <form className="bg-white rounded-lg shadow-lg p-6 w-full md:w-2/3 lg:w-1/2 xl:w-1/3">
+    <div className="flex items-center justify-center w-full h-screen px-4 bg-blue-100">
+      <form
+        className="bg-white rounded-lg shadow-lg p-6 w-full md:w-2/3 lg:w-1/2 xl:w-1/3"
+        onSubmit={onSubmitHandler}
+      >
         <h2 className="text-2xl font-bold mb-6 text-center">로그인</h2>
         <div className="mb-4">
           <label className="block font-bold mb-2" htmlFor="username">
@@ -21,8 +43,10 @@ export default function SignIn() {
           <input
             className="border rounded-lg py-2 px-3 w-full"
             type="text"
-            name="username"
-            id="username"
+            name="id"
+            id="id"
+            value={id}
+            onChange={onIdHandler}
           />
         </div>
         <div className="mb-6">
@@ -34,6 +58,8 @@ export default function SignIn() {
             type="password"
             name="password"
             id="password"
+            value={password}
+            onChange={onPasswordHandler}
           />
         </div>
         <button
@@ -42,6 +68,9 @@ export default function SignIn() {
         >
           로그인
         </button>
+        {errorMessage && (
+          <div className="text-red-500 text-sm mt-2">{errorMessage}</div>
+        )}
       </form>
     </div>
   );
