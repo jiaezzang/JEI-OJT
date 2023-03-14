0a8369340b0c35702345806754c0e0bfc764ef36
JIAE-K <2023a0015@jei.com>
2023-03-14 11:00:01 +0900

    모바일 버전 버튼

--- ReactApp/vite-project/src/pages/Main/Main.tsx
+++ ReactApp/vite-project/src/pages/Main/Main.tsx
@@ -14,7 +14,7 @@ const Button = ({ label, onClick, value}: ButtonProps) => {
 
   return (
     <button
-    className="bg-blue-500 rounded-xl shadow-lg px-4 py-2 mr-2 text-white"
+    className="bg-violet-700 hover:bg-violet-400 rounded-xl shadow-lg px-4 py-2 mr-2 text-white w-full mt-4"
       onClick={onClick} value={value}
     >
       {label}
@@ -41,12 +41,12 @@ export default function Main() {
 
   return (
     <div
-      className="flex flex-col items-center justify-center w-full h-screen px-4 bg-blue-100" >
-      <div className="flex flex-row justify-between" style={{width: '100%', justifyContent: 'space-around'}}>
-        <h1 className="font-bold mr-2 text-2xl">안녕 {name}</h1>
+      className="flex flex-col items-center justify-center w-full h-screen px-4 bg-violet-100" >
+      <div className="max-w-screen-lg flex flex-row justify-between" style={{width: '150%', justifyContent: 'space-around'}}>
+        <h1 className="font-bold mr-2 text-xl md:text-2xl lg:text-3xl">안녕 {name}</h1>
         <img className='logout cursor-pointer w-10 h-10' src={logoutLogo} width="50" onClick={MoveToSignIn}/>
       </div>
-      <div className="p-2 md:p-2 w-full flex flex-col items-center max-w-screen-md">
+      <div className="p-2 md:p-2 w-full flex flex-col min-w-max max-w-screen-lg">
         <div className="w-full h-0 relative" style={{ paddingTop: '62.7%' }}>
           <iframe
             src={content}
@@ -56,12 +56,18 @@ export default function Main() {
             allowFullScreen
           ></iframe>
         </div>
-        <div className="flex mt-4" >
+        <div className="hidden md:flex items-center space-x-1" >
           <Button label="튜토리얼" onClick={btnHandler} value="http://127.0.0.1:5500/ReactApp/vite-project/public/content3/index.html"/>
           <Button label="더하기" onClick={btnHandler} value="http://127.0.0.1:5500/ReactApp/vite-project/public/content1/index.html"/>
           <Button label="원 그리기" onClick={btnHandler} value="http://127.0.0.1:5500/ReactApp/vite-project/public/content2/index.html"/>
           <Button label="삼각형 찾기" onClick={btnHandler} value="http://127.0.0.1:5500/ReactApp/vite-project/public/content4/index.html"/>
         </div>
+      </div>
+      <div className="md:hidden flex flex-col items-center w-full" >
+        <Button label="튜토리얼" onClick={btnHandler} value="http://127.0.0.1:5500/ReactApp/vite-project/public/content3/index.html"/>
+        <Button label="더하기" onClick={btnHandler} value="http://127.0.0.1:5500/ReactApp/vite-project/public/content1/index.html"/>
+        <Button label="원 그리기" onClick={btnHandler} value="http://127.0.0.1:5500/ReactApp/vite-project/public/content2/index.html"/>
+        <Button label="삼각형 찾기" onClick={btnHandler} value="http://127.0.0.1:5500/ReactApp/vite-project/public/content4/index.html"/>
       </div>
     </div>
   );
