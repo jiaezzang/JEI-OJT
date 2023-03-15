dda23a07c1cae8321723b12348885153b261fe4c
JIAE-K <2023a0015@jei.com>
2023-03-15 16:08:42 +0900

    도넛차트 추가

--- ReactApp/vite-project/src/pages/MyPage/MyPage.tsx
+++ ReactApp/vite-project/src/pages/MyPage/MyPage.tsx
@@ -4,11 +4,16 @@ import logoutLogo from '../../assets/img/logout_icon.png';
 import studyLogo from '../../assets/img/study_icon.png';
 import NavBar from '../../components/NavBar';
 import Modal from '../../components/Modal';
+import Chart from '../../components/Chart';
 
 export default function MyPage() {
   //학습 페이지에서 데이터 전송받기
   const location = useLocation();
-  const { name } = location.state;
+  const name = location.state.name;
+  const correct = location.state.correct;
+
+  console.log(correct);
+  console.log(name);
 
   //페이지 이동
   const navigate = useNavigate();
@@ -40,9 +45,10 @@ export default function MyPage() {
   return (
     <React.Fragment>
        <Modal open={modalOpen} close={closeModal} header={header} submit={submitEvent}>{modalMsg}</Modal>
-      <div className="flex flex-col items-center justify-center w-full h-screen px-4 bg-violet-100">
+      <div className="flex flex-col items-center justify-center w-full h-screen bg-violet-100">
         <NavBar src1={studyLogo} onClick1={MoveToMain} src2={logoutLogo} onClick2={MoveToSignIn} name={name}/>
-        <div className="bg-white rounded-lg shadow-lg p-6 w-full md:w-2/3 lg:w-1/2 xl:w-1/3">
+        <div className="flex justify-center bg-white rounded-lg shadow-lg p-6 h-3/4 w-full md:w-2/3 lg:w-1/2 xl:w-5/7">
+          <Chart correct={correct}></Chart>
         </div>
 
       </div>
