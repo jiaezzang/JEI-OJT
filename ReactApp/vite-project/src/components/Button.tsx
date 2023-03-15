c574e386109ca0182d07f0b3870fcd38d18fb7bf
jiaezzang <visveryver2@gmail.com>
2023-03-15 11:26:43 +0900

    Button 컴포넌트 분리

---
+++ ReactApp/vite-project/src/components/Button.tsx
@@ -0,0 +1,19 @@
+interface ButtonProps {
+    label: string;
+    onClick?: (e: any) => void;
+    value: string;
+  }
+  
+  const Button = ({ label, onClick, value}: ButtonProps) => {
+  
+    return (
+      <button
+      className="bg-violet-700 hover:bg-violet-400 rounded-xl shadow-lg px-4 py-2 mr-2 text-white w-full mt-4"
+        onClick={onClick} value={value}
+      >
+        {label}
+      </button>
+    );
+  };
+
+  export default Button
--- ReactApp/vite-project/src/pages/Main/Main.tsx
+++ ReactApp/vite-project/src/pages/Main/Main.tsx
@@ -4,24 +4,8 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import logoutLogo from '../../assets/img/logout_icon.png';
 import graphLogo from '../../assets/img/graph_icon.png';
 import Modal from '../../components/Modal'
+import Button from '../../components/Button'
 
-interface ButtonProps {
-  label: string;
-  onClick?: (e: any) => void;
-  value: string;
-}
-
-const Button = ({ label, onClick, value}: ButtonProps) => {
-
-  return (
-    <button
-    className="bg-violet-700 hover:bg-violet-400 rounded-xl shadow-lg px-4 py-2 mr-2 text-white w-full mt-4"
-      onClick={onClick} value={value}
-    >
-      {label}
-    </button>
-  );
-};
 
 let num = 0, count = 0;
 
@@ -113,45 +97,46 @@ export default function Main() {
   }
 
   return (
-    <div
-      className="flex flex-col items-center justify-center w-full h-screen px-4 bg-violet-100" >
+    <React.Fragment>
       <Modal open={modalOpen} close={closeModal} submit={submitEvent} header={header}>{modalMsg}</Modal>
-      <div className="max-w-screen-lg flex flex-row justify-between" style={{width: '150%', justifyContent: 'space-around'}}>
-        <h1 className="font-bold mr-2 text-xl md:text-2xl lg:text-3xl justify-contentflex-start">안녕 {name}</h1>
-        <div className="flex justify-contantflex-end space-x-2">
-          {/* <img className='logout cursor-pointer w-10 h-10' src={graphLogo} width="50" onClick={MoveToMyPage}/> */}
-          <img className='logout cursor-pointer w-10 h-10 mt-1' src={logoutLogo} width="50" onClick={MoveToSignIn}/>
+      <div className="flex flex-col items-center justify-center w-full h-screen px-4 bg-violet-100">
+        <div className="max-w-screen-lg flex flex-row justify-between" style={{width: '150%', justifyContent: 'space-around'}}>
+          <h1 className="font-bold mr-2 text-xl md:text-2xl lg:text-3xl justify-contentflex-start">안녕 {name}</h1>
+          <div className="flex justify-contantflex-end space-x-2">
+            {/* <img className='logout cursor-pointer w-10 h-10' src={graphLogo} width="50" onClick={MoveToMyPage}/> */}
+            <img className='logout cursor-pointer w-10 h-10 mt-1' src={logoutLogo} width="50" onClick={MoveToSignIn}/>
+          </div>
         </div>
-      </div>
-      <div className="p-2 md:p-2 w-full flex flex-col min-w-max max-w-screen-md">
-        <div className="w-full h-0 relative" style={{ paddingTop: '62.7%' }}>
-          <iframe
-            src={content}
-            className="absolute top-1 left-0 w-full h-full shadow-lg bg-white"
-            frameBorder="0"
-            scrolling="no"
-            allowFullScreen
-          ></iframe>
-        </div>
+        <div className="p-2 md:p-2 w-full flex flex-col min-w-max max-w-screen-md">
+          <div className="w-full h-0 relative" style={{ paddingTop: '62.7%' }}>
+            <iframe
+              src={content}
+              className="absolute top-1 left-0 w-full h-full shadow-lg bg-white"
+              frameBorder="0"
+              scrolling="no"
+              allowFullScreen
+            ></iframe>
+          </div>
 
-        
-        <div className="hidden md:flex flex-col" >
-        <Button label="순서대로 학습" onClick={btnHandler} value="/content3/index.html"/>
-          <div className='flex'>
+          
+          <div className="hidden md:flex flex-col" >
+            <Button label="순서대로 학습하기" onClick={btnHandler} value="/content3/index.html"/>
+            <div className='flex'>
+              <Button label="튜토리얼" onClick={btnHandler} value="/content3/index.html"/>
+              <Button label="더하기" onClick={btnHandler} value="/content1/index.html"/>
+              <Button label="원 그리기" onClick={btnHandler} value="/content2/index.html"/>
+              <Button label="삼각형 찾기" onClick={btnHandler} value="/content4/index.html"/>
+            </div>
+          </div>
+        </div>
+        <div className="md:hidden flex flex-col w-4/5" >
+          <Button label="순서대로 학습하기" onClick={btnHandler} value="/content3/index.html"/>
           <Button label="튜토리얼" onClick={btnHandler} value="/content3/index.html"/>
           <Button label="더하기" onClick={btnHandler} value="/content1/index.html"/>
           <Button label="원 그리기" onClick={btnHandler} value="/content2/index.html"/>
           <Button label="삼각형 찾기" onClick={btnHandler} value="/content4/index.html"/>
-          </div>
         </div>
       </div>
-      <div className="md:hidden flex flex-col w-4/5" >
-        <Button label="순서대로 학습" onClick={btnHandler} value="/content3/index.html"/>
-        <Button label="튜토리얼" onClick={btnHandler} value="/content3/index.html"/>
-        <Button label="더하기" onClick={btnHandler} value="/content1/index.html"/>
-        <Button label="원 그리기" onClick={btnHandler} value="/content2/index.html"/>
-        <Button label="삼각형 찾기" onClick={btnHandler} value="/content4/index.html"/>
-      </div>
-    </div>
+    </React.Fragment>
   );
 }
