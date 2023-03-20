5a8379468b994767cd0d78351f388f92327ca692
JIAE-K <2023a0015@jei.com>
2023-03-20 15:05:36 +0900

    fakeAPI

--- ReactApp/vite-project/src/api/user.tsx
+++ ReactApp/vite-project/src/api/user.tsx
@@ -1,19 +1,32 @@
 import axios from 'axios';
 import AxiosMockAdapter from "axios-mock-adapter";
-
-export function fetchUsers() {
-    const mock = new AxiosMockAdapter(axios);
+import { createFakeAPI } from './mock';
 
 
-    const data = {
-        users:[
-            { id: 'jiae22', password: '1234', name: '지애' },
-            { id: 'jiae33', password: '3456', name: 'jiae' },
-            { id: 'jiae44', password: '4567', name: 'kim' },
-        ]   
-    };
+export const postSignIn = (id:string,password:string) =>
+    axios.post("/sign-in",{id,password})
 
-    mock.onGet('/users').reply(200, data)
+  createFakeAPI(axios)  
+
+// export function fetchUsers() {
+//     const mock = new AxiosMockAdapter(axios);
+
+//     const data = {
+//         users:[
+//             { id: 'jiae22', password: '1234', name: '지애' },
+//             { id: 'jiae33', password: '3456', name: 'jiae' },
+//             { id: 'jiae44', password: '4567', name: 'kim' },
+//         ]   
+//     };
+
+//     mock.onGet('/users').reply(() => {
+//         try {
+//             return [200, data];
+//         } catch(error) {
+//             console.error(error);
+//             return [500, {message : "error!!"}]
+//         }
+//     })
   
-    return axios.get('/users')
-}
+//     return axios.get('/users')
+// }
