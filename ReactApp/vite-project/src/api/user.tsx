1892b11c0f4ecef701b0962fa177068ea43a3afb
jiaezzang <visveryver2@gmail.com>
2023-03-21 10:58:21 +0900

    주석 삭제

--- ReactApp/vite-project/src/api/user.tsx
+++ ReactApp/vite-project/src/api/user.tsx
@@ -8,26 +8,3 @@ export const postSignIn = (id:string, password:string) =>
   axiosInstance.post("/sign-in",{id, password})
 
   createFakeAPI(axiosInstance)  
-
-// export function fetchUsers() {
-//     const mock = new AxiosMockAdapter(axios);
-
-//     const data = {
-//         users:[
-//             { id: 'jiae22', password: '1234', name: '지애' },
-//             { id: 'jiae33', password: '3456', name: 'jiae' },
-//             { id: 'jiae44', password: '4567', name: 'kim' },
-//         ]   
-//     };
-
-//     mock.onGet('/users').reply(() => {
-//         try {
-//             return [200, data];
-//         } catch(error) {
-//             console.error(error);
-//             return [500, {message : "error!!"}]
-//         }
-//     })
-  
-//     return axios.get('/users')
-// }
